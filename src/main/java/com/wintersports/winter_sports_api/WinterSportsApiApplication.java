4c6bdf009249b4a676897d0811d721f375b9c68f
iv-kovatchev <iv.kovatchev@gmail.com>
2026-03-31 00:43:42 +0300

    setup skeleton

---
+++ src/main/java/com/wintersports/winter_sports_api/WinterSportsApiApplication.java
@@ -0,0 +1,13 @@
+package com.wintersports.winter_sports_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinterSportsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinterSportsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wintersports/winter_sports_api/WinterSportsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wintersports.winter_sports_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinterSportsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
